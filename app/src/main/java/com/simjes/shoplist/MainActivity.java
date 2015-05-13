72342b04a12a5f99f314a7ad3b966cf253a8003c
Simon Jespersen <s.jespersen@stud.uis.no>
2015-05-14 00:27:43 +0200

    title and icon showing

--- app/src/main/java/com/simjes/shoplist/MainActivity.java
+++ app/src/main/java/com/simjes/shoplist/MainActivity.java
@@ -1,12 +1,9 @@
 package com.simjes.shoplist;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
--- app/src/main/java/com/simjes/shoplist/SwipeListener.java
+++ app/src/main/java/com/simjes/shoplist/SwipeListener.java
@@ -18,8 +18,8 @@ public class SwipeListener implements OnTouchListener {
     }
 
     private final class GestureListener extends SimpleOnGestureListener {
-        private static final int SWIPE_THRESHOLD = 100;
-        private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+        private static final int SWIPE_THRESHOLD = 50;
+        private static final int SWIPE_VELOCITY_THRESHOLD = 50;
 
         @Override
         public boolean onDown(MotionEvent e) {
