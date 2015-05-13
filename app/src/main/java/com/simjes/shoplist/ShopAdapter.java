f78faf9a23620b8ef967a0ed0bb105ea4e478e9d
Simon Jespersen <s.jespersen@stud.uis.no>
2015-05-13 17:15:56 +0200

    swipe to delete

--- app/src/main/java/com/simjes/shoplist/ShopAdapter.java
+++ app/src/main/java/com/simjes/shoplist/ShopAdapter.java
@@ -1,6 +1,7 @@
 package com.simjes.shoplist;
 
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.Adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,7 +9,8 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class ShopAdapter extends RecyclerView.Adapter<ShopAdapter.ViewHolder> {
+public class ShopAdapter extends Adapter<ShopAdapter.ViewHolder> {
+
     private ArrayList<String> dataset;
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
@@ -27,8 +29,16 @@ public class ShopAdapter extends RecyclerView.Adapter<ShopAdapter.ViewHolder> {
     }
 
     @Override
-    public ShopAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ShopAdapter.ViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.customtextview, parent, false);
+        v.setOnTouchListener(new SwipeListener(parent.getContext(), v) {
+            @Override
+            public void onSwipeRight() {
+                TextView tv = (TextView) view.findViewById(R.id.itemTextView);
+                dataset.remove(tv.getText().toString());
+                notifyDataSetChanged();
+            }
+        });
         ViewHolder vh = new ViewHolder(v);
         return vh;
     }
