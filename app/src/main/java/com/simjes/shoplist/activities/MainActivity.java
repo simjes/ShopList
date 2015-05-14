1b01629e812c2260310f28340fc2f373672deef6
Simon Jespersen <s.jespersen@stud.uis.no>
2015-05-14 23:03:51 +0200

    save list when closing app

--- app/src/main/java/com/simjes/shoplist/activities/MainActivity.java
+++ app/src/main/java/com/simjes/shoplist/activities/MainActivity.java
@@ -1,6 +1,7 @@
 package com.simjes.shoplist.activities;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -11,10 +12,17 @@ import com.simjes.shoplist.listeners.KeyboardListener;
 import com.simjes.shoplist.R;
 import com.simjes.shoplist.adapters.ShopAdapter;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class MainActivity extends Activity {
+    private static final String FILENAME = "ShopListItems";
+
     private RecyclerView.Adapter adapter;
     private ArrayList<String> dataset;
     private EditText editText;
@@ -35,6 +43,49 @@ public class MainActivity extends Activity {
         recyclerView.setAdapter(adapter);
         editText = (EditText) findViewById(R.id.inputField);
         editText.setOnEditorActionListener(new KeyboardListener(this));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        String allItems = "";
+        try {
+            FileInputStream fileInputStream = openFileInput(FILENAME);
+            byte[] itemsInBytes = new byte[fileInputStream.available()];
+            if (itemsInBytes.length > 0) {
+                while (fileInputStream.read(itemsInBytes) != -1) {
+                }
+                allItems += new String(itemsInBytes);
+                String[] allItemsArray = allItems.split(";");
+                dataset.addAll(Arrays.asList(allItemsArray));
+            }
+            fileInputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        String allItems = "";
+        for (String s : dataset) {
+            allItems += s + ";";
+        }
+
+        try {
+            FileOutputStream fileOutputStream = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+            fileOutputStream.write(allItems.getBytes());
+            fileOutputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     /*@Override
